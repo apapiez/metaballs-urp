bf9def4e9f8c26c28530b8654efded60a1600c59
apapiez <alex.papiez@googlemail.com>
2022-07-30 10:31:21 +0100

    xxx

---
+++ Assets/oscillator.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class oscillator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //oscilate the object
+        transform.position = new Vector3(Mathf.Sin(Time.time * 2), 0, 0);
+        
+
+
+
+        
+    }
+}
